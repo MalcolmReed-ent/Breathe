3d3c98b39376820feda6bb567b160c54811883b8
Malcolm Reed <malcolmreed2151@protonmail.com>
2024-10-06 13:26:52 -0400

    Update config.h

--- config.h
+++ config.h
@@ -1,14 +1,68 @@
-/*
- * Background color, X11 color name (see: https://en.wikipedia.org/wiki/X11_color_names).
- */
-static const char *bg_color = "Gray50";
+#ifndef CONFIG_H
+#define CONFIG_H
 
-/*
- * Parameters are: mask, keysym, action.
- * Mask can be any mask from XKeyEvent.state (see: man XKeyEvent) or
- * AnyMask (mask is ignored), or EmptyMask (empty mask, no key modifiers).
- * Keysym can be any keysym from /usr/include/X11/keysymdef.h.
- */
+#include <X11/keysym.h>
+
+/* General Settings */
+static const char *font = "-misc-fixed-medium-r-normal-*-14-*-*-*-*-*-*-*";
+static const char *bg_color = "#2E3440";  // Nord theme background color
+
+/* Scrolling and Zooming */
+static const double arrow_scroll = 0.01;
+static const double page_scroll  = 0.30;
+static const double mouse_scroll = 0.02;
+static const double zoom_step = 1.1;
+static const double min_zoom = 0.1;
+static const double max_zoom = 5.0;
+static const double default_zoom = 1.0;
+
+/* Selection */
+static const unsigned long selection_color = 0x5E81AC;  // Nord theme blue
+
+/* Rotation */
+static const int default_rotation = 0;
+
+/* Features */
+static const int enable_text_selection = 1;
+static const int enable_link_following = 1;
+static const int cache_size_mb = 64;
+
+/* View Modes */
+static const int default_two_page_view = 0;
+static const int default_continuous_mode = 0;
+
+/* Status Bar */
+static const int default_show_status_bar = 1;
+static const int status_bar_height = 20;
+static const char *status_bar_font = "-misc-fixed-medium-r-normal-*-13-*-*-*-*-*-*-*";
+
+/* Color Scheme */
+static const int default_dark_mode = 0;
+
+// Light mode colors (Nord-inspired)
+static const char *status_bar_text_color_light = "#2E3440";  // Nord Polar Night
+static const char *status_bar_bg_color_light = "#D8DEE9";    // Nord Snow Storm
+
+// Dark mode colors (Nord theme)
+static const char *status_bar_text_color_dark = "#ECEFF4";   // Nord Snow Storm
+static const char *status_bar_bg_color_dark = "#3B4252";     // Nord Polar Night
+static const char *page_bg_color_dark = "#d4b595";           // Nord Polar Night (darker)
+
+/* Keyboard Shortcuts */
+typedef enum {
+    QUIT, NEXT, PREV, FIRST, LAST, FIT_PAGE, FIT_WIDTH,
+    DOWN, UP, PG_DOWN, PG_UP, BACK, RELOAD, COPY,
+    GOTO_PAGE, SEARCH, PAGE, MAGNIFY, ROTATE_CW, ROTATE_CCW,
+    ZOOM_IN, ZOOM_OUT, TOGGLE_TWO_PAGE_VIEW,
+    TOGGLE_CONTINUOUS_MODE, TOGGLE_STATUS_BAR, TOGGLE_DARK_MODE
+} Action;
+
+typedef struct {
+    unsigned mask;
+    KeySym ksym;
+    Action action;
+} Shortcut;
+
 static const Shortcut shortcuts[] = {
     {AnyMask,     XK_q,            QUIT},
     {EmptyMask,   XK_Escape,       QUIT},
@@ -31,63 +85,13 @@ static const Shortcut shortcuts[] = {
     {EmptyMask,   XK_p,            PAGE},
     {EmptyMask,   XK_m,            MAGNIFY},
     {EmptyMask,   XK_bracketright, ROTATE_CW},
-    {EmptyMask,   XK_bracketleft,  ROTATE_CCW}
+    {EmptyMask,   XK_bracketleft,  ROTATE_CCW},
+    {ControlMask, XK_plus,         ZOOM_IN},
+    {ControlMask, XK_minus,        ZOOM_OUT},
+    {EmptyMask,   XK_t,            TOGGLE_TWO_PAGE_VIEW},
+    {EmptyMask,   XK_c,            TOGGLE_CONTINUOUS_MODE},
+    {EmptyMask,   XK_F7,           TOGGLE_STATUS_BAR},
+    {EmptyMask,   XK_i,            TOGGLE_DARK_MODE}
 };
 
-/*
- * Scrolling speed (in page fractions).
- */
-static const double arrow_scroll = 0.01;
-static const double page_scroll  = 0.30;
-static const double mouse_scroll = 0.02;
-
-/*
- * Font for any text rendering, must be in X logical font description format
- * (see: https://en.wikipedia.org/wiki/X_logical_font_description).
- */
-static const char *font = "-misc-fixed-medium-r-normal-*-14-*-*-*-*-*-*-*";
-
-/*
- * Selection color (RGB format)
- */
-static const unsigned long selection_color = 0x7F7F7F; // Light gray
-
-/*
- * Default zoom level when opening a new document (1.0 = 100%)
- */
-static const double default_zoom = 1.0;
-
-/*
- * Maximum zoom level
- */
-static const double max_zoom = 5.0;
-
-/*
- * Minimum zoom level
- */
-static const double min_zoom = 0.1;
-
-/*
- * Zoom step for zoom in/out operations
- */
-static const double zoom_step = 0.1;
-
-/*
- * Default rotation angle (in degrees, must be a multiple of 90)
- */
-static const int default_rotation = 0;
-
-/*
- * Enable or disable text selection (1 = enabled, 0 = disabled)
- */
-static const int enable_text_selection = 1;
-
-/*
- * Enable or disable link following (1 = enabled, 0 = disabled)
- */
-static const int enable_link_following = 1;
-
-/*
- * Cache size (in MB) for storing rendered pages
- */
-static const int cache_size_mb = 64;
+#endif // CONFIG_H
